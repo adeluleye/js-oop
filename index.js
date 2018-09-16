de8bb16bb2e101dc76f311deac6aefe81ae8154b
adeluleye <adelu4real@gmail.com>
2018-09-16 23:33:13 +0100

    creating objects using object literals and factories

---
+++ index.js
@@ -0,0 +1,33 @@
+console.log('Hello World');
+
+// creating Object using Object Literals
+const circle = {
+  radius: 1,
+  location: {
+    x: 1,
+    y: 1
+  },
+  draw: function() {
+    console.log('draw');
+  }
+};
+
+circle.draw();
+
+// using Factories
+function createCircle(radius) {
+  return {
+    radius,
+    draw: function() {
+      console.log('draw');
+    }
+  };
+}
+
+const circle2 = createCircle(1);
+const circle3 = createCircle(2);
+const circle4 = createCircle(3);
+
+console.log(circle2.radius);
+console.log(circle3.radius);
+console.log(circle4.radius);
